13ad04c1da2474f8227475a6fd6cf0224c58b722
Brett Turing <bjsherman80@gmail.com>
2020-07-08 10:43:40 -0600

    Started percentage occupied method

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,10 +1,11 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :percentage_occupied
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.to_i
     @products = []
     @total_number_of_products = 0
+    @percentage_occupied = 0
   end
 
   def add_product(item)
@@ -25,4 +26,8 @@ class ShoppingCart
     else
       false
     end
+
+  def percentage_occupied
+    @percentage_occupied = (@total_number_of_products.to_i / 30) * 100
   end
+end
