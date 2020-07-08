26bea1cbb3f3200697b7e7aa51e0bef88532c182
Brett Turing <bjsherman80@gmail.com>
2020-07-08 10:34:28 -0600

    Add total number of products & full cart method

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -4,12 +4,25 @@ class ShoppingCart
     @name = name
     @capacity = capacity.to_i
     @products = []
+    @total_number_of_products = 0
   end
 
   def add_product(item)
     @products << item
   end
 
+  def total_number_of_products
+    products.each do |item|
+      total_number_of_products << item[4]
+    end
+    total_number_of_products.each { |a| sum += a}
+    end
+  end
 
-
-end
+  def is_full
+    if @total_number_of_products == 30
+      true
+    else
+      false
+    end
+  end
