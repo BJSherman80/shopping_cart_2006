c71862a67111ee9cf82b8d7b5ec653817094c52a
Brett Turing <bjsherman80@gmail.com>
2020-07-08 10:48:15 -0600

    Tryed to add percent full method; broke code I deleted

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,11 +1,10 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products, :percentage_occupied
+  attr_reader :name, :capacity, :products
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.to_i
     @products = []
     @total_number_of_products = 0
-    @percentage_occupied = 0
   end
 
   def add_product(item)
@@ -26,8 +25,4 @@ class ShoppingCart
     else
       false
     end
-
-  def percentage_occupied
-    @percentage_occupied = (@total_number_of_products.to_i / 30) * 100
   end
-end
