d59d734db022ee163ad8a74ee2d949e5ab95b731
Brett Turing <bjsherman80@gmail.com>
2020-07-08 10:02:33 -0600

    Create all  tests for shopping cart class

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -12,7 +12,33 @@ class ShoppingCartTest < Minitest::Test
     assert_instance_of ShoppingCart, cart
   end
 
+  def test_it_has_a_name
+    cart = ShoppingCart.new("King Soopers", "30items")
 
+    assert_equal "King Soopers", cart.name
+  end
 
+  def test_it_has_a_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
 
+    assert_equal 30, cart.capacity
+  end
+
+  def test_it_has_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal [], cart.products
+  end
+
+  def test_it_can_add_products
+
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    assert_equal [product1, product2],cart.products
+  end
 end
